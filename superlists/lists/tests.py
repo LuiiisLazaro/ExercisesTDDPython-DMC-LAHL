87bb1a417df430929f47cfecfed6d8142af495d1
LuiiisLazaro <luishdezlazaro@hotmail.com>
2015-10-12 18:27:40 -0500

    Mi premier Modelo de base de datos y migracioón asociada

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,28 @@
+from lists.models import Item
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'La primera Lista de Elementos'
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'El segundo Elemento'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'La primera Lista de Elementos')
+        self.assertEqual(second_saved_item.text, 'El segundo Elemento')
 
 # Create your tests here.
 class HomePageTest(TestCase):
