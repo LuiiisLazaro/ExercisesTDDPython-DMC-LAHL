dfc5b6edd294d24e88c5879a414d6626eed25354
LuiiisLazaro <luishdezlazaro@hotmail.com>
2015-10-12 13:37:13 -0500

    Refactorizando vista de página de inicio a usar como Plantilla | pág 45

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,17 +5,19 @@ from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
-	"""docstring for ClassName"""
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    """docstring for ClassName"""
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do list</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
 
 """class SmokeTest(TestCase):
 	def test_bad_maths(self):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 #home_page = None
 def home_page(request):
-	return HttpResponse('<html><title>To-Do list</title></html>')
+    return render(request, 'home.html')
+    #return HttpResponse('<html><title>To-Do list</title></html>')
