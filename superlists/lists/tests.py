db35a52139646217999399241c2d88d813f716d0
LuiiisLazaro <luishdezlazaro@hotmail.com>
2015-10-12 18:47:38 -0500

    commit para página 66 assertionError 0!=1

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -46,6 +46,11 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = 'a new list item'
 
         response = home_page(request)
+        
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'a new list item')
+
 
         self.assertIn('a new list item', response.content.decode())
         expected_html = render_to_string('home.html', {'new_item_text': 'a new list item'})
