b9d8199c0ed99d0965a519fc81d7c41edd793c0b
danielmendez88 <daniboymendez@gmail.com>
2015-10-13 15:47:36 -0500

    pruebas aisladas | pag 79

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Daniel ha escuchado acerca de una nueva aplicación genial en línea "to-do app"
         #Él va ha checar esta página.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
 	#Él ve el titulo de la página y el encabezado mencinando "To-Do lists"
         self.assertIn('To-Do', self.browser.title)
@@ -66,7 +66,5 @@ class NewVisitorTest(unittest.TestCase):
 
 	#satisfecho, el va a dormir.
 
-if __name__=='__main__':
-    unittest.main(warnings='ignore')
 browser.quit()
 
