21cc4e652376d8c3a156880e940db149752869a7
LuiiisLazaro <luishdezlazaro@hotmail.com>
2015-10-14 16:30:30 -0500

    probando vistas, plantillas y urls juntas con el cliente de pruebas de Django | pág 89

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,96 +2,91 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    
     def tearDown(self):
         self.browser.quit()
-
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-
     def test_can_start_a_list_and_retrieve_it_later(self):
-        #Daniel ha escuchado acerca de una nueva aplicación genial en línea "to-do app"
-        #Él va ha checar esta página.
+        # Daniel ha escuchado acerca de una nueva aplicacion genial en linea "to-do app"
+        # Él va ha checar esta página.
         self.browser.get(self.live_server_url)
 
-    #Él ve el titulo de la página y el encabezado mencinando "To-Do lists"
+        # Él ve el titulo de la página y el encabezado mencinando "To-Do lists"
         self.assertIn('To-Do', self.browser.title)
+
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-    #Él es invitado a ingresar un item directamente a "To-Do list"
+        # Él es invitado a ingresar un item directamente a "To-Do list"
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'), 
-            'Ingresa un Elemento a To-Do'
-        )
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Ingresa un Elemento a To-Do')
 
-    #El escribe "comprar plumas de pavo" dentro de la caja de texto, El hobby de Daniel es hacer señuelos para pesca.
+        # El escribe "comprar plumas de pavo" dentro de la caja de texto, El hobby de Daniel es hacer señuelos para pesca.
         inputbox.send_keys('comprar plumas de pavo')
 
-    #Cuando el da enter, la página se actualiza y ahora la lista de la página contiene un item
-    #llamado "1: comprar plumas de pavo real".
+        # Cuando el da enter, la página se actualiza y ahora la lista de la página contiene un item
+        # llamado "1: comprar plumas de pavo real".
         inputbox.send_keys(Keys.ENTER)
-        #editar
+
         daniel_list_url = self.browser.current_url
-        self.assertRegex(daniel_list_url,'/lists/.+')
-       
-	#todavia hay una caja de texto invitandole a agregar otro item. el 
-	# ingresa "usar plumas y pavo para hacer señuelo de pesca"
-        #un usuario llego, Daniel se fue por galletas
+        self.assertRegex(daniel_list_url, '/lists/.+')
+        self.check_for_row_in_list_table("1: comprar plumas de pavo")
+
+        # todavia hay una caja de texto invitandole a agregar otro item. el
+        # ingresa "usar plumas y pavo para hacer señuelo de pesca"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('usar las plumas de pavo')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table("1: comprar plumas de pavo")
+        self.check_for_row_in_list_table("2: usar las plumas de pavo")
+
+        # un usuario llego, Daniel se fue por galletas
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        #Luis visita la pagina
+        # Luis visita la pagina
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('comprar plumas de pavo', page_text)
         self.assertNotIn('usar las plumas de pavo', page_text)
 
-        #Luis inicia una nueva lista ingresando un nuevo elemento
-        inputbox3 = self.browser.find_element_by_id('id_new_item')
-        inputbox3.send_keys('comprar leche')
-        inputbox3.send_keys(Keys.ENTER)
-        #luis obtiene su propia url
+        # Luis inicia una nueva lista ingresando un nuevo elemento
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('comprar leche')
+        inputbox.send_keys(Keys.ENTER)
+        # luis obtiene su propia url
         luis_list_url = self.browser.current_url
-        self.assertRegex(luis_list_url,'/lists/.+')
-        self.assertNotEqual(daniel_list_url, luis_list_url)
+        self.assertRegex(luis_list_url, '/lists/.+')
+        self.assertNotEqual(luis_list_url, daniel_list_url)
 
-        #nuevamente, no hay rastro de la lista de daniel
+        # nuevamente, no hay rastro de la lista de daniel
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('comprar plumas de pavo', page_text)
         self.assertIn('comprar leche', page_text)
-        #luis se va a dormir
-
-        self.check_for_row_in_list_table("1: comprar plumas de pavo")
-
-        inputbox2 = self.browser.find_element_by_id('id_new_item')
-        inputbox2.send_keys('usar las plumas de pavo')
-        inputbox2.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table("2: usar las plumas de pavo")
+        # luis se va a dormir
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1: comprar plumas de pavo', [row.text for row in rows])
         self.assertIn('2: usar las plumas de pavo', [row.text for row in rows])
-    #todavia hay una caja de texto invitandole a agregar otro item. el
-    # ingresa "usar plumas y pavo para hacer señuelo de pesca"
+        # todavia hay una caja de texto invitandole a agregar otro item. el
+        # ingresa "usar plumas y pavo para hacer señuelo de pesca"
         self.fail('Prueba Finalizada :D ')
 
-    #la página se actualiza nuevamente y nos muestra dos elementos en la lista.
+        # la página se actualiza nuevamente y nos muestra dos elementos en la lista.
 
-    #Daniel se pregunta si el sitio recordará su lista. Entonces se percata que el sitio
-    #ha generado una unica URL para el, --- hay alguna explicación para ese efecto.
+        # Daniel se pregunta si el sitio recordará su lista. Entonces se percata que el sitio
+        # ha generado una unica URL para el, --- hay alguna explicación para ese efecto.
 
-    #El visita esa URl --- su lista "To-Do" todavia se encuentra ahí
-    #satisfecho, el va a dormir.
-
-
+        # El visita esa URl --- su lista "To-Do" todavia se encuentra ahí
+        # satisfecho, el va a dormir.
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -24,6 +24,7 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'La primera Lista de Elementos')
         self.assertEqual(second_saved_item.text, 'El segundo Elemento')
 
+
 # Create your tests here.
 class HomePageTest(TestCase):
     """docstring for ClassName"""
@@ -63,7 +64,6 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        print(response['location'])
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
@@ -80,3 +80,14 @@ class HomePageTest(TestCase):
 
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
+
+
+class ListViewTest (TestCase):
+    def test_display_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
