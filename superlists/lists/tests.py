798d7ee0ca3e3ac2febfcb09415831524d80cf0f
LuiiisLazaro <luishdezlazaro@hotmail.com>
2015-10-14 16:43:41 -0500

    Hasta aquí la plantilla no existe pero los métodos de prueba ya estan declarados | pág 91

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,25 +5,6 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 
-class ItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'La primera Lista de Elementos'
-        first_item.save()
-        
-        second_item = Item()
-        second_item.text = 'El segundo Elemento'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'La primera Lista de Elementos')
-        self.assertEqual(second_saved_item.text, 'El segundo Elemento')
-
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -66,23 +47,25 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+    #def test_home_page_displays_all_list_items(self):
+        #Item.objects.create(text='itemey 1')
+        #Item.objects.create(text='itemey 2')
+        #request = HttpRequest()
+        #response = home_page(request)
+        #self.assertIn('itemey 1', response.content.decode())
+        #self.assertIn('itemey 2', response.content.decode())
 
 
 class ListViewTest (TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
     def test_display_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
@@ -91,3 +74,25 @@ class ListViewTest (TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'La primera Lista de Elementos'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'El segundo Elemento'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'La primera Lista de Elementos')
+        self.assertEqual(second_saved_item.text, 'El segundo Elemento')
+
+
