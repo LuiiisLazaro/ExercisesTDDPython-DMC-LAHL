d83dfd8f5c7aca9b250593cc93745a1787edfa97
danielmendez88 <daniboymendez@gmail.com>
2015-10-16 14:37:20 -0500

    primeros pasos de layout esperando error | página 118

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -100,12 +100,14 @@ class NewVisitorTest(LiveServerTestCase):
         # El visita esa URl --- su lista "To-Do" todavia se encuentra ahí
         # satisfecho, el va a dormir.
     def test_layout_and_styling(self):
-        #daniel va a la pagina de inicio
+        # daniel va a la pagina de inicio
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024,768)
 
-        #nota que en la caja de texto es centrado
-        inputbox = self.browser.find_element_by_id('id_new_tem')
+        # nota que en la caja de texto es centrado
+        # centraremos esto también
+        # inputbox.send_keys('prueba\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
