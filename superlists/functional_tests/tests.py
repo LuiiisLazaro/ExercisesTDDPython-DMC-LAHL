e101eac336279e279722cc23b3dc8c537c3a2873
LuiiisLazaro <luishdezlazaro@hotmail.com>
2015-10-16 14:58:25 -0500

    Primeros pasos de pruebas funcionales para diseño y estilo | ch-7 | pág 188

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -99,14 +99,24 @@ class NewVisitorTest(LiveServerTestCase):
 
         # El visita esa URl --- su lista "To-Do" todavia se encuentra ahí
         # satisfecho, el va a dormir.
+
     def test_layout_and_styling(self):
         # daniel va a la pagina de inicio
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024,768)
+        self.browser.set_window_size(1024, 768)
 
         # nota que en la caja de texto es centrado
         # centraremos esto también
-        # inputbox.send_keys('prueba\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # Daniel incia una lista
+        # Centrada también
+        inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
